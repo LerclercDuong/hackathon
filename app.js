c9cdfc59a6ea6a0f4c8979b386e266f3690806e8
minhtri <doantri2003@gmail.com>
2022-11-26 12:45:13 +0700

    2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port =  3000
+const port = process.env.PORT || 3000;
 const db = require('./src/config/db/db')
 const route = require('./src/routes/index')
 var bodyParser = require('body-parser')
@@ -14,5 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 db.connect()
 route(app)
+app.get('/', function(req, res){
+    res.json({message:'none'})
+})
 // console.log(process.env.PORT)
 app.listen( port)
